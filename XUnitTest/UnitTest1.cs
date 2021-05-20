7655082eebf36fd8bd8329eef3a28c5fe879e233
archi-Doc <archidoc422@gmail.com>
2021-05-20 23:57:33 +0900

    .

--- XUnitTest/UnitTest1.cs
+++ XUnitTest/UnitTest1.cs
@@ -1,6 +1,7 @@
 // Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
 using System;
+using CrossChannel;
 using Xunit;
 
 namespace XUnitTest
@@ -10,6 +11,21 @@ namespace XUnitTest
         [Fact]
         public void Test1()
         {
+            using (var c = Radio.OpenTwoWay<int, int>(x => x * 2))
+            {
+                Radio.SendTwoWay<int, int>(1).IsStructuralEqual(new int[] { 2 });
+                Radio.SendTwoWay<int, int>(2).IsStructuralEqual(new int[] { 4 });
+
+                using (var d = Radio.OpenTwoWay<int, int>(x => x * 2))
+                {
+                    Radio.SendTwoWay<int, int>(1).IsStructuralEqual(new int[] { 2, 2 });
+                    Radio.SendTwoWay<int, int>(2).IsStructuralEqual(new int[] { 4, 4 });
+                }
+
+                Radio.SendTwoWay<int, int>(3).IsStructuralEqual(new int[] { 6 });
+            }
+
+            Radio.SendTwoWay<int, int>(4).IsStructuralEqual(new int[] { });
         }
     }
 }
