c9d063ba3d6f3f1cd4c2c3ce2a2c7f421e22bd72
archi-Doc <archidoc422@gmail.com>
2024-08-13 18:43:30 +0900

    .

--- CrossChannelGenerator/CrossChannelObject.cs
+++ CrossChannelGenerator/CrossChannelObject.cs
@@ -1,8 +1,5 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
-using System;
-using System.Collections.Immutable;
-using System.Runtime.InteropServices;
 using System.Text;
 using Arc.Visceral;
 using Microsoft.CodeAnalysis;
