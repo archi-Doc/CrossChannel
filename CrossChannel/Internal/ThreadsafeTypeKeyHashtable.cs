2466da1b66bb7a44cbb3f73c1a52aba19cdcc0ba
archi-Doc <archidoc422@gmail.com>
2024-08-20 11:03:26 +0900

    .

--- CrossChannel/Internal/ThreadsafeTypeKeyHashtable.cs
+++ CrossChannel/Internal/ThreadsafeTypeKeyHashtable.cs
@@ -1,40 +1,101 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
-#pragma warning disable SA1202 // Elements should be ordered by access
 #pragma warning disable SA1214 // Readonly fields should appear before non-readonly fields
+#pragma warning disable SA1401 // Fields should be private
 
 namespace CrossChannel.Internal;
 
 internal class ThreadsafeTypeKeyHashtable<TValue>
 {
+    private const double LoadFactor = 0.75d;
+
+    private readonly object syncObject = new();
     private Entry[] buckets;
     private int size; // only use in writer lock
 
-    private readonly object writerLock = new object();
-    private readonly float loadFactor;
-
-    public ThreadsafeTypeKeyHashtable(int capacity = 4, float loadFactor = 0.75f)
+    private class Entry
     {
-        var tableSize = CalculateCapacity(capacity, loadFactor);
+        public Entry(Type key, TValue value, int hash)
+        {
+            this.Key = key;
+            this.Value = value;
+            this.Hash = hash;
+        }
+
+        internal Type Key;
+        internal TValue Value;
+        internal int Hash;
+        internal Entry? Next;
+    }
+
+    public ThreadsafeTypeKeyHashtable(int capacity = 4)
+    {
+        var tableSize = CalculateCapacity(capacity);
         this.buckets = new Entry[tableSize];
-        this.loadFactor = loadFactor;
     }
 
     public bool TryAdd(Type key, TValue value)
-    {
-        return this.TryAdd(key, _ => value);
-    }
+        => this.TryAdd(key, _ => value);
 
     public bool TryAdd(Type key, Func<Type, TValue> valueFactory)
+        => this.TryAddInternal(key, valueFactory, out TValue _);
+
+    public TValue GetOrAdd(Type key, Func<Type, TValue> valueFactory)
     {
-        return this.TryAddInternal(key, valueFactory, out TValue _);
+        TValue? v;
+        if (this.TryGetValue(key, out v))
+        {
+            return v;
+        }
+
+        this.TryAddInternal(key, valueFactory, out v);
+        return v;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool TryGetValue(Type key, [MaybeNullWhen(false)] out TValue value)
+    {
+        Entry[] table = this.buckets;
+        var hash = key.GetHashCode();
+        Entry? entry = table[hash & table.Length - 1];
+
+        while (entry != null)
+        {
+            if (entry.Key == key)
+            {
+                value = entry.Value;
+                return true;
+            }
+
+            entry = entry.Next;
+        }
+
+        value = default;
+        return false;
+    }
+
+    private static int CalculateCapacity(int collectionSize)
+    {
+        var initialCapacity = (int)(collectionSize / LoadFactor);
+        var capacity = 1;
+        while (capacity < initialCapacity)
+        {
+            capacity <<= 1;
+        }
+
+        if (capacity < 8)
+        {
+            return 8;
+        }
+
+        return capacity;
     }
 
     private bool TryAddInternal(Type key, Func<Type, TValue> valueFactory, out TValue resultingValue)
     {
-        lock (this.writerLock)
+        lock (this.syncObject)
         {
-            var nextCapacity = CalculateCapacity(this.size + 1, this.loadFactor);
+            var nextCapacity = CalculateCapacity(this.size + 1);
 
             if (this.buckets.Length < nextCapacity)
             {
@@ -126,73 +187,5 @@ internal class ThreadsafeTypeKeyHashtable<TValue>
         }
 
         return true;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool TryGetValue(Type key, [MaybeNullWhen(false)] out TValue value)
-    {
-        Entry[] table = this.buckets;
-        var hash = key.GetHashCode();
-        Entry? entry = table[hash & table.Length - 1];
-
-        while (entry != null)
-        {
-            if (entry.Key == key)
-            {
-                value = entry.Value;
-                return true;
-            }
-
-            entry = entry.Next;
-        }
-
-        value = default;
-        return false;
-    }
-
-    public TValue GetOrAdd(Type key, Func<Type, TValue> valueFactory)
-    {
-        TValue? v;
-        if (this.TryGetValue(key, out v))
-        {
-            return v;
-        }
-
-        this.TryAddInternal(key, valueFactory, out v);
-        return v;
-    }
-
-    private static int CalculateCapacity(int collectionSize, float loadFactor)
-    {
-        var initialCapacity = (int)(((float)collectionSize) / loadFactor);
-        var capacity = 1;
-        while (capacity < initialCapacity)
-        {
-            capacity <<= 1;
-        }
-
-        if (capacity < 8)
-        {
-            return 8;
-        }
-
-        return capacity;
-    }
-
-    private class Entry
-    {
-        public Entry(Type key, TValue value, int hash)
-        {
-            this.Key = key;
-            this.Value = value;
-            this.Hash = hash;
-        }
-
-#pragma warning disable SA1401 // Fields should be private
-        internal Type Key;
-        internal TValue Value;
-        internal int Hash;
-        internal Entry? Next;
-#pragma warning restore SA1401 // Fields should be private
     }
 }
