0237ccf6592e84852dbc6dccd1f7fe1eecfda689
archi-Doc <archidoc422@gmail.com>
2024-08-21 22:24:02 +0900

    .

--- CrossChannel/GeneratorShared/IRadioService.cs
+++ CrossChannel/GeneratorShared/IRadioService.cs
@@ -4,7 +4,8 @@ namespace CrossChannel;
 
 /// <summary>
 /// Represents a base interface used as a Radio service.<br/>
-/// The requirements are to add the <see cref="RadioServiceInterfaceAttribute" /> and to derive from the <see cref="IRadioService" />.
+/// The requirements are to add the <see cref="RadioServiceInterfaceAttribute" /> and to derive from the <see cref="IRadioService" />.<br/>
+/// The return type of the interface function must be either <see cref="void"/>, <see cref="Task"/>, <see cref="RadioResult{T}"/>, <see cref="Task{T}"/>(where TResult is <see cref="RadioResult{T}"/>).
 /// </summary>
 public interface IRadioService
 {
